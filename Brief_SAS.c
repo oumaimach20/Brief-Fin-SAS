8af41bf496721f425f8068590eec63c8290216a9
oumaimach20 <157011249+oumaimach20@users.noreply.github.com>
2024-01-29 00:10:57 +0100

    Add files via upload
    Création de Menu

---
+++ Brief_SAS.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	char choix;
+	//Afficher le menu
+	printf("   Bienvenue dans le logiciel de gestion des taches(:\n");
+	printf("\n        Menu :\n");
+	printf("\n1.Ajouter une tache.\n");
+	printf("2.Afficher la liste des taches.\n");
+	printf("3.Modifier une tache.\n");
+	printf("4.Supprimer une tache.\n");
+	printf("5.Ordonner les taches.\n");
+	printf("6.Filtrer les taches.\n");
+	printf("7.Quitter.\n");
+	//Demander à l'utilisateur de choisir
+	printf("\nVeuillez entrer votre choix:");
+	scanf("%c", &choix);
+	
+
+
+
+}
